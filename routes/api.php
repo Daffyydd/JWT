8d2d16741c68fccd6f0457d142621d44feb2f7bc
Daffyydd <dnyakundi@gmail.com>
2021-11-25 18:21:59 +0000

    Initial commit from PHPSandbox

---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\JwtAuthController;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/register', [JwtAuthController::class, 'register']);
+    Route::post('/login', [JwtAuthController::class, 'login']);
+    Route::get('/user-profile', [JwtAuthController::class, 'user']);
+    Route::post('/token-refresh', [JwtAuthController::class, 'refresh']);
+    Route::post('/logout', [JwtAuthController::class, 'logout']);
+});
